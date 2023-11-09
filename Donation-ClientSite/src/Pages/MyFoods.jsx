a3c20af66075cfa63972a1b12a6174b3befb8a2b
RashedaAkther <rashedaakther006@gmail.com>
2023-11-10 00:55:18 +0600

    first commit

---
+++ Donation-ClientSite/src/Pages/MyFoods.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const MyFoods = () => {
+  return (
+    <div>
+      <Helmet><title>My_Foods</title></Helmet>
+    </div>
+  );
+};
+
+export default MyFoods;
